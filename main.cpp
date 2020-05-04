39e44e8ce75b7a0ca5f91b89f83a3cc989c11521
Christopher Lu <chlu@rita-temp.cecs.pdx.edu>
2020-05-03 21:15:09 -0700

    fixed destructor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 //This is main!
 //
 #include "queue.h"
-
+#include <iostream>
 using namespace std;
 
 int main()
 {
     queue queue;
+    char address[20];
+    int choice;
+    cout << "Programming assingment #2"
+        << endl << "#1: Create an address:"
+        << endl << "0 to Exit" << endl;
+        cin >> choice;
+        cin.ignore(100, '\n'); 
+   do{
+        if(choice == 1)
+        {
+            cout << "#1: Create a address" << endl;
+            cout << "address name:";
+            cin.get(address, 20, '\n');
+            cin.ignore(100, '\n');
+        }
+
+   }
+   while(choice != 0);
+   cout << "Exiting" << endl;
     return 1;
 }
--- queue.cpp
+++ queue.cpp
@@ -13,21 +13,55 @@
     
    queue::~queue()
    {
-    if(rear->next != nullptr)
+    if(rear) //this is segfaulting
     {
-        address * temp = rear;
+        address * temp = rear;  
         rear=rear->next;
-        
+        delete temp; //not sure the order
     } 
+       //recursively advance here
+   }; //This is the default destructor; //do we want it to be a wrapper?
+  
+    /* This is a Queue: 
+     * (rear) -------- > (front)
+     */
 
-   }; //This is the default destructor;
+    //This function puts data onto the rear of the queue
+    int queue::enqueue(const address & address_toadd ) //This function puts data into the rear of the queue.
+    {
+    //Guard Case: Is full 
+        
+    //copy address_toadd into temp
+    address * temp = new address;                   
+    temp->addressStr = address_toadd.addressStr;        //make a new node + copy the parm into it
+ 
+    //Case 1: Empty Queue (if null)
+    if(rear == nullptr)
+    {
+        rear->next = temp; 
+    }
     
-    int enqueue(const address &); //This function puts data 
-                                    //into the rear of the queue.
-   
-    //int dequeue(const address &); //This function removes data 
-                                    //off the front of the queue.
-    //isEmpty();                //Returns 1 if the queue is empty.
-	//isFull();                 //Returns 1 if the queue is full.
-    //int peek(address &);      //Returns the top of the queue by reference
-	//int displayQueue();           //Hopefully it prints from 
+    //Case 2: 1 item (in the event it points to itself)
+    else if(rear->next == rear) 
+    {   
+        temp->next = rear;
+        rear->next = temp;
+    
+    }
+    //Case 3: More items (+1)
+    else 
+    {
+    address * temp = rear->next; 
+    rear->next = new address;
+    rear = rear->next;
+    rear->next = temp;
+    }
+    
+    return 1;
+    }
+//int dequeue(const address &); //This function removes data 
+//off the front of the queue.
+//isEmpty();                //Returns 1 if the queue is empty.
+//isFull();                 //Returns 1 if the queue is full.
+//int peek(address &);      //Returns the top of the queue by reference
+//int displayQueue();           //Hopefully it prints from 
